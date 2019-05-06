b300197ca4ce73bcb78328535749bac5e989d33d
Tate <smd.tate@gmail.com>
2019-05-06 13:41:53 +0800

    add styled-components

--- src/components/index.js
+++ src/components/index.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Button from '@material-ui/core/Button';
 import { hot } from 'react-hot-loader';
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: orange;
+  font-size: 18px;
+`
 
 function Counter({
   counter, increment, decrement
 }) {
     return (
-      <p>
-        Holy shit Clicked: {counter} times
-        {' '}
-        <button onClick={increment}>
-          +
-        </button>
-        {' '}
-        <button onClick={decrement}>
-          -
-        </button>
-        <Button variant="contained" color="primary">
-          Hello Tate & Snow
-        </Button>
-      </p>
+      <Fragment>
+        <Title>Good morning</Title>
+        <p>
+          Holy shit Clicked: {counter} times
+          {' '}
+          <button onClick={increment}>
+            +
+          </button>
+          {' '}
+          <button onClick={decrement}>
+            -
+          </button>
+          <Button variant="contained" color="primary">
+            Hello Tate & Snow
+          </Button>
+        </p>
+      </Fragment>
     )
 }
 
