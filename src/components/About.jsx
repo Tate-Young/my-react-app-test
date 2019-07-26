a42e388c9a8347072cd58e326d981da6cafbfc8a
Tate <smd.tate@gmail.com>
2019-07-26 15:10:41 +0800

    feat(HOC): finish

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,33 @@
 import React, { Component } from "react";
+import { withContent } from "../common";
 
-export class About extends Component {
-  state = {
-    content: "Loading…"
-  }
+// export class About extends Component {
+//   state = {
+//     content: "Loading…"
+//   }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then(response => response.json())
-      .then(data => this.setState({ content: data.title }))
-      .catch(() => this.setState({ content: "Error" }))
-  }
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/todos/1")
+//       .then(response => response.json())
+//       .then(data => this.setState({ content: data.title }))
+//       .catch(() => this.setState({ content: "Error" }))
+//   }
 
-  render() {
-    return (
-      <article>
-        <h1>Content</h1>
-        <p>{this.state.content}</p>
-      </article>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <article>
+//         <h1>Content</h1>
+//         <p>{this.state.content}</p>
+//       </article>
+//     )
+//   }
+// }
+
+const Func = ({ content }) => (
+  <article>
+    <h1>Content</h1>
+    <p>{content}</p>
+  </article>
+)
+
+export const About = withContent(Func)
